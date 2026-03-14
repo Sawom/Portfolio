79dbb6be96c4de6bc2e15fdf594b2789c77f3398
Sawom <asawom250@gmail.com>
2026-03-15 05:16:14 +0600

    icons added in skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,103 +1,214 @@
 import { motion } from "framer-motion";
-import { FaBootstrap, FaCss3Alt, FaFigma, FaHtml5, FaJsSquare, FaNodeJs, FaPython } from "react-icons/fa";
+// import { AiFillAntDesign } from "react-icons/ai";
+import {
+  FaBootstrap,
+  FaCss3Alt,
+  FaFigma,
+  FaHtml5,
+  FaJsSquare,
+  FaNodeJs,
+  FaPython,
+} from "react-icons/fa";
 import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
-import { SiDaisyui, SiExpress, SiFirebase, SiMongodb, SiMysql, SiTypescript } from "react-icons/si";
+import {
+  SiDaisyui,
+  SiExpress,
+  SiFirebase,
+  SiJsonwebtokens,
+  SiMongodb,
+  SiMui,
+  SiMysql,
+  SiPostgresql,
+  SiPrisma,
+  SiTypescript,
+} from "react-icons/si";
 import { TbBrandCpp, TbSquareLetterCFilled } from "react-icons/tb";
 
-
-const iconvarriant = (duration) =>({
-    initial:{y:-10},
-    animate:{
-        y : [10 , -10],
-        transition:{
-            duration: duration,
-            ease:"linear",
-            repeat: Infinity,
-            repeatType:"reverse"
-        },
+const iconvarriant = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
     },
+  },
 });
 
 const Tech = () => {
   return (
     <div className="border-b border-neutral-800 pb-4">
-        <motion.h2 
-            whileInView={{opacity:1 , y:0}} 
-            initial={{opacity:0 , y:-100}} 
-            transition={{duration:1.5}}
-        className="my-20 text-center text-4xl">Skills</motion.h2>
-        <div className="flex flex-wrap items-center justify-center gap-6">
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaHtml5 className="text-7xl text-orange-500" />
-                HTML
-            </motion.div>
-            <motion.div variants={iconvarriant(2)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaCss3Alt className="text-7xl text-blue-500" />
-                CSS
-            </motion.div>
-            <motion.div variants={iconvarriant(2)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaBootstrap className="text-7xl text-purple-500" />
-                Bootstrap
-            </motion.div>
-            <motion.div  variants={iconvarriant(2.5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <RiTailwindCssFill className="text-7xl text-blue-400" />
-                Tailwind
-            </motion.div>
-            <motion.div  variants={iconvarriant(2.5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiDaisyui className="text-7xl text-green-300" />
-                Daisyui
-            </motion.div>
-            <motion.div variants={iconvarriant(2.5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-                React js
-            </motion.div>
-            <motion.div variants={iconvarriant(3)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaNodeJs className="text-7xl text-green-500" />
-                Node js
-            </motion.div> 
-            <motion.div variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaJsSquare className="text-7xl text-yellow-400" />
-                JavaScript
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiTypescript className="text-7xl text-blue-500" />
-                Typescript
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaPython className="text-7xl text-blue-400" />
-                Python
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <TbBrandCpp className="text-7xl text-blue-600" />
-                C++
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <TbSquareLetterCFilled className="text-7xl text-blue-600" />
-                C
-            </motion.div>
-            <motion.div variants={iconvarriant(4)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiMongodb className="text-7xl text-green-400" />
-                MongoDB
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiMysql className="text-7xl text-blue-400" />
-                Mysql
-            </motion.div>
-            <motion.div  variants={iconvarriant(2.5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiExpress className="text-7xl text-green-500" />
-                Express
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <SiFirebase className="text-7xl text-red-400" />
-                Firebase
-            </motion.div>
-            <motion.div  variants={iconvarriant(5)} animate="animate" initial="initial" className="rounded-2xl text-center border-4 p-4 border-neutral-800">
-                <FaFigma className="text-7xl text-orange-400 " />
-                Figma
-            </motion.div>
-        </div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Skills
+      </motion.h2>
+      <div className="flex flex-wrap items-center justify-center gap-6">
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaHtml5 className="text-7xl text-orange-500" />
+          HTML
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaCss3Alt className="text-7xl text-blue-500" />
+          CSS
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaBootstrap className="text-7xl text-purple-500" />
+          Bootstrap
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2.5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <RiTailwindCssFill className="text-7xl text-blue-400" />
+          Tailwind
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2.5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiDaisyui className="text-7xl text-green-300" />
+          Daisyui
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2.5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+          React js
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(3)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+          Node js
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaJsSquare className="text-7xl text-yellow-400" />
+          JavaScript
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiTypescript className="text-7xl text-blue-500" />
+          Typescript
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaPython className="text-7xl text-blue-400" />
+          Python
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <TbBrandCpp className="text-7xl text-blue-600" />
+          C++
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <TbSquareLetterCFilled className="text-7xl text-blue-600" />C
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(4)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiMongodb className="text-7xl text-green-400" />
+          MongoDB
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiMysql className="text-7xl text-blue-400" />
+          Mysql
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(2.5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiExpress className="text-7xl text-green-500" />
+          Express
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <SiFirebase className="text-7xl text-red-400" />
+          Firebase
+        </motion.div>
+        <motion.div
+          variants={iconvarriant(5)}
+          animate="animate"
+          initial="initial"
+          className="rounded-2xl text-center border-4 p-4 border-neutral-800"
+        >
+          <FaFigma className="text-7xl text-orange-400 " />
+          Figma
+        </motion.div>
+        <SiPostgresql />
+        <SiPrisma />
+        <SiJsonwebtokens />
+        {/* <AiFillAntDesign /> */}
+        <SiMui />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Tech;
